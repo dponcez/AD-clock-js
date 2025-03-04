393367007a8e1f3d5d60e6c6112aa2d1d484e9dc
Damian Ponce <dponcez_git@hotmail.com>
2025-03-03 22:31:15 -0500

    feat: update weekInfoTemplate to display current month and enhance time formatting

--- modules/time_format.js
+++ modules/time_format.js
@@ -12,7 +12,7 @@ export const weekInfoTemplate = `
       <p class="date color" data-date>0</p>
     </div>
     <div class="date--container flexbox">
-      <p class="month color" data-month>march</p>
+      <p class="month color" data-month>february</p>
     </div>
   </div>
 `;
@@ -20,16 +20,20 @@ export const weekInfoTemplate = `
 export const currentTimeFormat = () => {
   const htmlRefs = {
     dataDay: selector('[data-day]'),
-    dataDate: selector('[data-date]')
+    dataDate: selector('[data-date]'),
+    dataMonth: selector('[data-month]')
   }
   
-  const { dataDay, dataDate } = htmlRefs;
+  const { dataDay, dataDate, dataMonth } = htmlRefs;
   const WEEKS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   const CURRENT_WEEK = WEEKS[CURRENT_DATE.getDay()];
   const CURRENT_DAY = CURRENT_DATE.getDate();
+  const CURRENT_MONTH = MONTHS[CURRENT_DATE.getMonth()];
   
   dataDay.textContent = CURRENT_WEEK;
   dataDate.textContent = CURRENT_DAY;
+  dataMonth.textContent = CURRENT_MONTH;
 }
 
 export const formatInfoTemplate = `
